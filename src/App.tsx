a60a667d2cf081c8b9fc8aa3ba7d1a0a19000ee8
Le Hoai Nam <lehoainam0403@HoaiNam-Le.local>
2025-01-16 20:32:40 +0900

    change react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 
 import { publicRoutes } from './routes';
 import DefaultLayout from './layouts/DefaultLayout';
